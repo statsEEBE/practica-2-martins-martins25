a7b0b2245cf76ef3513bfc8cf7eb3d0b0be628fa
martin.salva <martin.salva@auleseebe.upc.edu>
2025-02-27 13:14:06 +0100

    ...

--- Problema2.R
+++ Problema2.R
@@ -19,3 +19,17 @@ b <- mean(y)-m*mean(x)
 b
 #llavors la recta de regressió és y=mx+b=0,4x+4,3
 m*1.5+b
+
+
+mod <- lm(y~x)
+summary(mod)
+ypredict <- predict(mod, data.frame(x=x))
+plot(x,y,pch=16)
+lines(x, ypredict,col='red')
+
+#r^2 determina la presició de la predicció: r^2=1 ( la predicció es exacta) r^2=0 (predicció inexacta)
+
+Rsq=sum((ypredict-mean(y))^2)/sum((y-mean(y))^2)
+Rsq
+
+summary(mod)
